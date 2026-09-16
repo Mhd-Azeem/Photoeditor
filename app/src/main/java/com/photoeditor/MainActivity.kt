b7e9216d6ea9b551fd6ffd3ca5659d6418cf8735
Mhd-Azeem <azeemzahira111@gmail.com>
2026-09-16 18:16:31 +0530

    feat: check and install app updates from MainActivity

--- app/src/main/java/com/photoeditor/MainActivity.kt
+++ app/src/main/java/com/photoeditor/MainActivity.kt
@@ -7,18 +7,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
 import com.photoeditor.ui.navigation.PhotoEditorNavGraph
 import com.photoeditor.ui.theme.PhotoEditorTheme
+import com.photoeditor.update.AppUpdater
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var appUpdater: AppUpdater
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         window.statusBarColor = android.graphics.Color.BLACK
         window.navigationBarColor = android.graphics.Color.BLACK
+
+        appUpdater = AppUpdater(this)
+
         setContent {
             PhotoEditorTheme {
                 PhotoEditorNavGraph()
             }
         }
+
+        appUpdater.checkForUpdates()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::appUpdater.isInitialized) {
+            appUpdater.installPendingIfAllowed()
+        }
+    }
+
+    override fun onDestroy() {
+        if (::appUpdater.isInitialized) {
+            appUpdater.dispose()
+        }
+        super.onDestroy()
     }
 }
